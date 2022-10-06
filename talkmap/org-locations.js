1e33f057d19b5f2218e5f404653ad5325e120467
Hang Haotian <hanghaotian@gmail.com>
2022-10-06 15:28:00 -0700

    update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Phoenix Convention Center, Phoenix, Arizona",
+    33.44936465000001,
+    -112.06891468239075
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Atlanta, Georgia",
+    33.7489924,
+    -84.3902644
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "UCLA, Los Angeles, California",
+    34.070877749999994,
+    -118.44685070595054
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Virtual, CT",
+    41.3948436,
+    -73.0732185
   ]
 ];
